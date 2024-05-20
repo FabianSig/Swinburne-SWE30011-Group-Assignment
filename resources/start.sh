5ec7966fdd1e2f4bda02f36b5c188ca549747d35
fabiansig <fabian.sigmund1@gmail.com>
2024-05-20 08:43:44 +0200

    fix scripts

--- resources/start.sh
+++ resources/start.sh
@@ -1,3 +1,1 @@
-pm2 start ../backend/app.js  --name nodeServer&
-sleep 2
-pm2 start ../backend/master_controller.py --interpreter python --name dbExport
+pm2 start ../Azure/backend/server.js  --name nodeServer

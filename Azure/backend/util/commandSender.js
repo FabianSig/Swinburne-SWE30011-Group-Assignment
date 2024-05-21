73a87913cf9d77b8073d9b654c2fae4bd00770af
fabiansig <fabian.sigmund1@gmail.com>
2024-05-21 05:51:40 +0200

    fix: commandSender

--- Azure/backend/util/commandSender.js
+++ Azure/backend/util/commandSender.js
@@ -2,28 +2,32 @@ const { getMQTTClient } = require('./mqttClient');
 
 var _thresholdTemperature = 30;
 
-function setThresholdTemperature(threshold){
-    _threshold = threshold
+function setThresholdTemperature(threshold) {
+  _thresholdTemperature = threshold;
 }
 
-function sendCommand(command){
-    const client = getMQTTClient();
-    if (client) {
-      client.publish('command/threshold', command, (err) => {
-        if (err) {
-          console.log('Failed to publish threshold value', err);
-          return res.status(500).send('Failed to publish threshold value');
-        }
-        res.send(`Command sent: ${command}`);
-      });
-    } else {
-      console.log('MQTT client is not connected');
-      res.status(500).send('MQTT client is not connected');
-    }
+function sendCommand(command) {
+  const client = getMQTTClient();
+  if (client) {
+    client.publish('command/threshold', command, (err) => {
+      if (err) {
+        console.log('Failed to publish threshold value', err);
+        return;
+      }
+      console.log(`Command sent: ${command}`);
+    });
+  } else {
+    console.log('MQTT client is not connected');
+  }
 }
 
-function checkCriticalValue(values){
-    if(values.temperature_values > _thresholdTemperature){
-        sendCommand("RED LED ON")
-    }
+function checkCriticalValue(values) {
+  if (values.temperature_values > _thresholdTemperature) {
+    sendCommand('RED LED ON');
+  }
 }
+
+module.exports = {
+  setThresholdTemperature,
+  checkCriticalValue,
+};
